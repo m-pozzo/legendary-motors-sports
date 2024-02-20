8a84c043277135b8fd397c8ae047e6cdd075ed98
Martin Pozzo <martinpozzo.tin@gmail.com>
2024-02-19 21:19:42 -0300

    button sort price

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import { Cars } from "./components/Cars/Cars"
 import { Footer } from "./components/Footer/Footer"
 import { Header } from "./components/Header/Header"
-import { useFetchData } from "./components/hooks/useFetchData"
 
 export const App = () => {
-
-    const { cars, setCar } = useFetchData([])
-
-
-    const sortCars = () => {
-        console.log(cars)
-        const autosOrdenados = [...cars].sort((a, b) => a.price - b.pricec)
-        setCar(autosOrdenados)
-        console.log(autosOrdenados)
-        console.log(cars)
-    }
 
     return (
         <div className="page_container">
             <Header></Header>
             <main className="main">
-                <section className="top_main">
-                    <h3>NOVEDADES</h3>
-                    <button className="btn_sort_price" onClick={sortCars}><span>Ordenar por precio</span><span>-</span></button>
-                </section>
                 <Cars></Cars>
             </main>
             <Footer></Footer>
--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,30 +1,54 @@
-import {useFetchData} from '../hooks/useFetchData'
+import { useFetchData } from '../hooks/useFetchData'
+import { useState } from 'react'
 
 export const Cars = () => {
 
-    const {cars} = useFetchData([])
+    const { cars, setCar } = useFetchData([])
+
+    const [filter, setFilter] = useState(true)
+
+    const sortCars = () => {
+        if (filter) {
+            let sortedCars = [...cars].sort((a, b) => a.price - b.price);
+            console.log(sortedCars)
+            setFilter(false)
+            setCar(sortedCars)
+        } else {
+            let sortedCars = [...cars].sort((a, b) => b.price - a.price);
+            console.log(sortedCars)
+            setFilter(true)
+            setCar(sortedCars)
+        }
+    }
+
 
     return (
-        <section className="cars_container">
-            {
-                cars.map(({ id, space, logo, name, price }) => {
-                    return (
-                        <article key={id} className="card">
-                            <div className="top_card">
-                                <img src="../src/assets/img/personDoor.png" alt="" className="door_person" />
-                                <span className="space_number">{space}</span>
-                            </div>
-                            <div className="car_img_container">
-                                <img src={logo} alt={name + " photo"} className="car_img" />
-                            </div>
-                            <div className="bottom_card">
-                                <h2 className="car_name">{name}</h2>
-                                <p className="car_price">${price.toLocaleString()}</p>
-                            </div>
-                        </article>
-                    )
-                })
-            }
-        </section>
+        <>
+            <section className="top_main">
+                <h3>NOVEDADES</h3>
+                <button className="btn_sort_price" onClick={sortCars}><span>Ordenar por precio</span><span>-</span></button>
+            </section>
+            <section className="cars_container">
+                {
+                    cars.map(({ id, space, logo, name, price }) => {
+                        return (
+                            <article key={id} className="card">
+                                <div className="top_card">
+                                    <img src="../src/assets/img/personDoor.png" alt="" className="door_person" />
+                                    <span className="space_number">{space}</span>
+                                </div>
+                                <div className="car_img_container">
+                                    <img src={logo} alt={name + " photo"} className="car_img" />
+                                </div>
+                                <div className="bottom_card">
+                                    <h2 className="car_name">{name}</h2>
+                                    <p className="car_price">${price.toLocaleString()}</p>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </section>
+        </>
     )
 }
