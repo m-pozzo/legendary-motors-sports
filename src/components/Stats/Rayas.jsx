b13389f16ffcf5d1b9efba8bf8b002a51ddd57d8
Martin Pozzo <martinpozzo.tin@gmail.com>
2024-03-02 16:44:29 -0300

    stats updated

--- src/components/Stats/Rayas.jsx
+++ src/components/Stats/Rayas.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-
 export const Rayas = ({ porcentaje }) => {
 
   const rayasLlenas = porcentaje / 20;
+  const rayaDecimal = rayasLlenas % 1.5;
+  const rayaRedondeada = rayaDecimal.toFixed(1)
+  console.log(rayasLlenas)
+
+  function tieneParteDecimal(numero) {
+    const numeroComoString = numero.toString();
+    const tienePunto = numeroComoString.includes('.');
+
+    if (tienePunto) {
+      const partes = numeroComoString.split('.');
+      const parteDecimal = partes[1];
+      return parteDecimal.length > 0;
+    }
+
+    return false;
+  }
+
+  const numeroDecimal = tieneParteDecimal(rayaDecimal)
 
   const renderRayas = () => {
     const rayas = [];
     for (let i = 0; i < 5; i++) {
-      rayas.push(
-        i <= rayasLlenas ? <span key={i} className='raya'></span> : <span className='raya_vacia'></span>
-      )
+      if (i === Math.floor(rayasLlenas)) {
+        rayas.push(
+          <span
+            key={i}
+            className='raya'
+            style={rayaRedondeada > 0 ? { width: rayaRedondeada * 20 + "px", marginRight: -rayaRedondeada * 20 + "px" } : {}}>
+          </span>
+        );
+      } else {
+        rayas.push(
+          <span key={i} className={i < Math.floor(rayasLlenas) ? 'raya' : 'raya_vacia'}>
+          </span>
+        );
+      }
     }
+    if (numeroDecimal) {
+      rayas.push(<span className='raya_vacia'></span>);
+      if (porcentaje >= 100) {
+        rayas.pop()
+      }
+    }
+
     return rayas;
   };
+
 
   return (
     <li>{renderRayas()}</li>
